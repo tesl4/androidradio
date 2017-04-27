6d21badfcb0153a0a9364b698a87ecacc9b5dd38
DESKTOP-FMBHDTU\tesl4 <tesl4@gmail.com>
2017-04-27 20:04:10 +0900

    20140427-2004

---
+++ AndroidApp/app/src/main/java/application/radio/tesl4/com/application/APlayList.java
@@ -0,0 +1,13 @@
+package application.radio.tesl4.com.application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class APlayList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a_aplay_list);
+    }
+}
